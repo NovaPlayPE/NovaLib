b6d60d65942bc7382fe1d99c791581aad57e55d2
Ragnok123 <gebab.urumci.kv@gmail.com>
2019-09-28 20:21:37 +0200

    Update ChatPacket.java

--- src/main/java/net/novaplay/networking/player/ChatPacket.java
+++ src/main/java/net/novaplay/networking/player/ChatPacket.java
@@ -9,6 +9,15 @@ import net.novaplay.networking.IPlayerPacket;
 public class ChatPacket extends Packet implements IPlayerPacket {
 
 	public String player;
+	/**
+	 * chat
+	 * tip
+	 * popup
+	 * title
+	 * subtitle
+	 * actionbar
+	 */
+	public String type = "chat";
 	public String message;
 	
 	@Override
@@ -17,6 +26,8 @@ public class ChatPacket extends Packet implements IPlayerPacket {
 		length = byteBuf.readInt();
 		player = (String) byteBuf.readCharSequence(length, Charsets.UTF_8);
 		length = byteBuf.readInt();
+		message = (String) byteBuf.readCharSequence(length,Charsets.UTF_8);
+		length = byteBuf.readInt();
 		message = (String) byteBuf.readCharSequence(length, Charsets.UTF_8);
 	}
 
@@ -24,6 +35,8 @@ public class ChatPacket extends Packet implements IPlayerPacket {
 	public void write(ByteBuf byteBuf) throws Exception {
 		byteBuf.writeInt(player.length());
 		byteBuf.writeCharSequence(player, Charsets.UTF_8);
+		byteBuf.writeInt(type.length());
+		byteBuf.writeCharSequence(type, Charsets.UTF_8);
 		byteBuf.writeInt(message.length());
 		byteBuf.writeCharSequence(message, Charsets.UTF_8);
 	}
