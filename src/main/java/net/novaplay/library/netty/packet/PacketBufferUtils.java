5fb79ff945927c444f6a239f00c0aa4bfb63fb60
Ragnok123 <gebab.urumci.kv@gmail.com>
2019-10-30 01:15:07 +0100

    You may ask
    Why I should write buffer utils?
    Answer: because it's indeed.

---
+++ src/main/java/net/novaplay/library/netty/packet/PacketBufferUtils.java
@@ -0,0 +1,5 @@
+package net.novaplay.library.netty.packet;
+
+public class PacketBufferUtils {
+
+}
---
+++ src/main/java/net/novaplay/networking/player/QuitPacket.java
@@ -0,0 +1,56 @@
+package net.novaplay.networking.player;
+
+import java.util.UUID;
+
+import com.google.common.base.Charsets;
+
+import io.netty.buffer.ByteBuf;
+import net.novaplay.library.netty.packet.Packet;
+import net.novaplay.networking.IPlayerPacket;
+
+public class QuitPacket extends Packet implements IPlayerPacket {
+
+	public String username;
+	public UUID uuid;
+	public String serverId;
+	public String reason;
+	
+	@Override
+	public void read(ByteBuf byteBuf) throws Exception {
+		int length;
+		length = byteBuf.readInt();
+		username = (String) byteBuf.readCharSequence(length,Charsets.UTF_8);
+		length = byteBuf.readInt();
+		uuid = getUuidFromString((String) byteBuf.readCharSequence(length,Charsets.UTF_8));
+		length = byteBuf.readInt();
+		serverId = (String) byteBuf.readCharSequence(length,Charsets.UTF_8);
+		length = byteBuf.readInt();
+		reason = (String) byteBuf.readCharSequence(length,Charsets.UTF_8);
+	}
+
+	@Override
+	public void write(ByteBuf byteBuf) throws Exception {
+		byteBuf.writeInt(username.length());
+		byteBuf.writeCharSequence(username, Charsets.UTF_8);
+		byteBuf.writeInt(getStringFromUuid(this.uuid).length());
+		byteBuf.writeCharSequence(getStringFromUuid(this.uuid), Charsets.UTF_8);
+		byteBuf.writeInt(serverId.length());
+		byteBuf.writeCharSequence(serverId, Charsets.UTF_8);
+		byteBuf.writeInt(reason.length());
+		byteBuf.writeCharSequence(reason,Charsets.UTF_8);
+	}
+	
+	public UUID getUuidFromString(String string) {
+		try {
+			return UUID.fromString(string);
+		} catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public String getStringFromUuid(UUID uuid) {
+		return uuid.toString();
+	}
+
+}
