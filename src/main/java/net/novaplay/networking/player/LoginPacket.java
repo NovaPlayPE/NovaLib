fd3bea1a5009125e1c20747a274caf046a601cb1
Ragnok123 <gebab.urumci.kv@gmail.com>
2020-01-05 17:55:39 +0100

    Update LoginPacket.java

--- src/main/java/net/novaplay/networking/player/LoginPacket.java
+++ src/main/java/net/novaplay/networking/player/LoginPacket.java
@@ -13,20 +13,17 @@ public class LoginPacket extends Packet implements IPlayerPacket{
 	
 	public String username;
 	public UUID uuid;
-	public String kickMessage;
 	public String serverId;
 	@Override
 	public void read(ByteBuf byteBuf) throws Exception {
 		username = PacketBufferUtils.readString(byteBuf);
 		uuid = PacketBufferUtils.readUUID(byteBuf);
-		kickMessage = PacketBufferUtils.readString(byteBuf);
 		serverId = PacketBufferUtils.readString(byteBuf);
 	}
 	@Override
 	public void write(ByteBuf byteBuf) throws Exception {
 		PacketBufferUtils.writeString(byteBuf,username);
 		PacketBufferUtils.writeUUID(byteBuf,uuid);
-		PacketBufferUtils.writeString(byteBuf,kickMessage);
 		PacketBufferUtils.writeString(byteBuf,serverId);
 	}
 
