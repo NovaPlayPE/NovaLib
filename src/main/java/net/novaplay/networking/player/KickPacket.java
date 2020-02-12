b048b52ec6e7528622eda2a8a55cf209afaf6968
Ragnok123 <gebab.urumci.kv@gmail.com>
2020-02-12 22:05:33 +0100

    Update KickPacket.java

--- src/main/java/net/novaplay/networking/player/KickPacket.java
+++ src/main/java/net/novaplay/networking/player/KickPacket.java
@@ -8,17 +8,20 @@ public class KickPacket extends Packet {
 	
 	public String player;
 	public String reason = "unknown";
+	public String type = "kick"; //there is also ban
 	
 	@Override
 	public void read(ByteBuf byteBuf) throws Exception {
 		player = PacketBufferUtils.readString(byteBuf);
 		reason = PacketBufferUtils.readString(byteBuf);
+		type = PacketBufferUtils.readString(byteBuf);
 	}
 
 	@Override
 	public void write(ByteBuf byteBuf) throws Exception {
 		PacketBufferUtils.writeString(byteBuf,player);
 		PacketBufferUtils.writeString(byteBuf,reason);
+		PacketBufferUtils.writeString(byteBuf,type);
 	}
 
 }
