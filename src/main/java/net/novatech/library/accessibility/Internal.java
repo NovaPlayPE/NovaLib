56dc0f4a54f5339665ce8736c612c06d62a0aedc
Ragnok123 <gebab.urumci.kv@gmail.com>
2022-01-21 21:57:15 +0100

    addition

---
+++ src/main/java/net/novatech/library/accessibility/Internal.java
@@ -0,0 +1,8 @@
+package net.novatech.library.accessibility;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.SOURCE)
+public @interface Internal {
+}
